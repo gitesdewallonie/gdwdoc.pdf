6fec8847b13b9d7f3827116b027f3507f049980b
Laurent Lasudry <lasudry@gmail.com>
2010-08-07 09:09:25 +0000

    Override aws.pdfbook to fix Plone 4 images insertion into PDF This refs #2471

--- gdwdoc/pdf/pdfgeneration.py
+++ gdwdoc/pdf/pdfgeneration.py
@@ -4,6 +4,10 @@ import os
 import tempfile
 from DateTime import DateTime
 from zope.app.component.interfaces import ISite
+from zope.app.component.hooks import getSite
+from Products.CMFCore.utils import getToolByName
+
+from aws.pdfbook import logger
 
 PDF_PREFIX = "Gites-de-Wallonie"
 
@@ -23,4 +27,65 @@ def initFileSystemInfo(self, context):
                                           filename_path,
                                           DateTime().strftime('%d-%m-%Y'))
     self.pdf_filepath = os.path.join(self.tmp_dir, self.pdf_filename)
+    return
+
+
+def saveImagesRecodeParser(self, context):
+    """Save images from ZODB to temp directory
+    """
+    portal = getSite()
+    portal_url = portal.absolute_url()
+    if not portal_url.endswith('/'):
+        portal_url += '/'
+    portal_path = '/'.join(portal.getPhysicalPath())
+    context_path = '/'.join(context.getPhysicalPath())
+
+    reference_tool = getToolByName(portal, 'reference_catalog')
+    for filename, image in self.images:
+
+        # Traverse methods mess with unicode
+        if type(image) is unicode:
+            image = str(image)
+        path = image.replace(portal_url, '')
+        #filename = os.path.basename(path)
+
+        item = None
+        # using uid
+        if image.find('resolveuid') != -1:
+            uuid = image.split('/')[-1]
+            item = reference_tool.lookupObject(uuid)
+            logger.debug("Get image from uid %s", uuid)
+
+        if not item:
+            # relative url
+            try:
+                item = context.restrictedTraverse(image)
+                logger.debug("Get image from context")
+            except:
+                logger.debug("Failed to get image from context path %s",
+                             image)
+        if not item:
+            # absolute url
+            image_path = '/'.join((portal_path, path))
+            try:
+                item = portal.restrictedTraverse(image_path)
+                logger.debug("Get image from portal")
+            except:
+                logger.debug("Failed to get image from portal path %s",
+                             image_path)
+                continue
+
+        # Eek, we should put an adapter for various image providers (overkill ?).
+        data = ''
+        print 'OK'
+        if item.meta_type in ('Portal Image', 'Image', 'ATBlob'):
+            data = item.data
+        elif item.meta_type == 'ATImage':
+            data = item.getImage()
+            data = getattr(item, 'data', data)
+
+        if data:
+            image_file = open(os.path.join(self.fsinfo.tmp_dir, filename), 'wb')
+            image_file.write(str(data))
+            image_file.close()
     return
